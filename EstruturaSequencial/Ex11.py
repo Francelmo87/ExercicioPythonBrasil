5cb67a8103b0151b1e6c10351fe8fce865f467b5
francelmo <francelmo87@gmail.com>
2022-08-20 19:31:09 -0300

    exercicios 11 corrigido o erro

--- EstruturaSequencial/Ex11.py
+++ EstruturaSequencial/Ex11.py
@@ -8,9 +8,9 @@ c. o terceiro elevado ao cubo."""
 num1 = int(input('Digite um número inteiro:'))
 num2 = int(input('Digite um outro numero inteiro: '))
 num3 = float(input('Digite um número real: '))
-a = ((num1 * 2 + (num2 / 2)) * (num1 * 2 + (num2 / 2)))
-b = ((3 * num1 + num3) + (3 * num1 + num3) + (3 * num1 + num3))
-c = num3**3
+a = (2 * num1) * (num2 / 2)
+b = (3 * num1) + num3
+c = num3 ** 3
 print(f'A. O produto do dobro do primeiro com metade do segundo é {a}')
 print(f'B. A soma do triplo do primeiro com o terceiro é {b}')
 print(f'C. O terceiro elevado ao cubo é {c}')
