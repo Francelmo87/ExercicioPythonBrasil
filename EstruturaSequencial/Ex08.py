94148016b0ae91dbd537e0d8dd6de0109aead67a
francelmo <francelmo87@gmail.com>
2022-08-20 18:50:51 -0300

    exercicios 08 corrigido o erro

--- EstruturaSequencial/Ex08.py
+++ EstruturaSequencial/Ex08.py
@@ -1,7 +1,7 @@
 """ Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
 Calcule e mostre o total do seu salário no referido mês."""
 
-salario = float(input('Qual é o valor de seu salário mensal: '))
+salario = float(input('Qual é o valor da hora trabalhada: '))
 horas = int(input('Quantas hora você trabalha mensalmente: '))
-result = salario / horas
+result = salario * horas
 print(f'você ganha {result :.2f} por {horas} horas trabalhada')
